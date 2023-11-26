e4defc57279866ca311d69ac701c5c96a245310a
Moaz <moazomran20@gmail.com>
2023-11-26 17:14:55 +0200

    league item added

--- lib/feature/search/presentation/view/widgets/search_tabs/leagues_tap.dart
+++ lib/feature/search/presentation/view/widgets/search_tabs/leagues_tap.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../../../../../home/presentation/view/widgets/league_details_item.dart';
+import '../league_item.dart';
+import '../search_team_item.dart';
 
 class LeaguesTap extends StatelessWidget {
   const LeaguesTap({Key? key}) : super(key: key);
@@ -10,12 +12,7 @@ class LeaguesTap extends StatelessWidget {
     return ListView.builder(itemBuilder: (context, index) {
       return const Padding(
         padding: EdgeInsets.only(bottom: 16),
-        child: LeagueDetailsItem(
-          leagueLogo: "assets/images/PremierLeague_logo_test.png",
-          leagueName: "Premier League",
-          countryLogo: "assets/images/country_icon_test.png",
-          countryName: "England",
-        ),
+        child: LeagueItem(),
       );
     },itemCount: 6,);
   }
