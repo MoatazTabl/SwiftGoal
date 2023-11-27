8d887afc01f97c73986c8b64947987bb4f7748df
Moaz <moazomran20@gmail.com>
2023-11-27 22:13:59 +0200

    matches tab added

--- lib/feature/team_details/presentation/view/widgets/tabs/matches_tab.dart
+++ lib/feature/team_details/presentation/view/widgets/tabs/matches_tab.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:swift_goal/feature/home/presentation/view/widgets/league_matches.dart';
 
 class MatchesTab extends StatelessWidget {
   const MatchesTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: ListView.builder(
+        itemBuilder: (context, index) {
+          return const LeagueMatchesItem();
+        },
+        itemCount: 2,
+      ),
+    );
   }
 }
